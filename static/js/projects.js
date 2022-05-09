79cdfee28e235407d84f5dea8ba9bd24e228cf34
Alessio Nardin <alessio.nardin@gmail.com>
2022-05-09 11:13:26 +0200

    improved js filtering

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,3 +1,16 @@
+var filterTags = function(tag) {
+	document.getElementById(tag).addEventListener("click", function() {
+		elements = document.getElementsByClassName("toggle-content");
+		for (let i = 0; i < elements.length; i++) {
+			if (elements[i].classList.contains(tag)){
+				show(elements[i]);
+			} else {
+				hide(elements[i]);
+			}
+		};
+	});
+};
+
 // Show an element
 var showAll = function (elements) {
 	for (let i = 0; i < elements.length; i++) {
@@ -5,14 +18,11 @@ var showAll = function (elements) {
 	};
 };
 
-
+var show = function(element){
+	element.classList.add('is-visible');
+}
 
 // Hide an element
-var hide = function (elem) {
-	elem.classList.remove('is-visible');
-};
-
-// Toggle element visibility
-var toggle = function (elem) {
-	elem.classList
+var hide = function (element) {
+	element.classList.remove('is-visible');
 };
